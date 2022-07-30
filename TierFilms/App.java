76ac30e3b5e1bf89ae2daf6416c7df7648f2bbba
Víctor Duplat Tigre <98099656+tigrevictorduplat@users.noreply.github.com>
2022-07-29 22:01:25 -0300

    Atualizações
    Operação de Listagem reduzida em Métodos. Generalização da conexão de APIs

--- TierFilms/App.java
+++ TierFilms/App.java
@@ -1,35 +1,13 @@
 package TierFilms;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.util.List;
-import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws IOException, InterruptedException {
-       //Conexão com a API
-       String urlAPI = "https://alura-filmes.herokuapp.com/conteudos";
-       URI uri = URI.create(urlAPI); 
-       var client = HttpClient.newHttpClient();
-       var request = HttpRequest.newBuilder(uri).GET().build();
-       HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-       //Extração  e Separação dos Dados
-       String apiBody = response.body();
-       JsonParser parser = new JsonParser();
-       List<Map<String,String>> listaFilmes = parser.parse(apiBody);
-       //Exibição dos Dados
-       for (Map<String,String> filmeListado : listaFilmes) {
-        System.out.println(filmeListado.get("rank")+"°");
-        System.out.println("Título: \u001b[107m\u001b[95m"+filmeListado.get("title")+"\u001b[m");
-        System.out.println("Nota: "+filmeListado.get("imDbRating"));
-        System.out.println(filmeListado.get("image"));
-        System.out.println();
-        System.out.println("-------------------");
-        
-
-       }
+      imdbAPIConnect listaTopFilmes = new imdbAPIConnect();
+      listaTopFilmes.listarTopFilmes(
+        listaTopFilmes.extractAPIBody(
+            listaTopFilmes.conectAPI(
+                "https://api.mocki.io/v2/549a5d8b")));
     }
 }
--- TierFilms/StarRank.java
+++ TierFilms/StarRank.java
@@ -2,32 +2,32 @@ package TierFilms;
 
 public class StarRank {
     public String covertRanktoStars(int rank) {
-         String stars ="";
+         String stars = "";
         switch (rank) {
             case 1:
             case 2:
-               stars = ""; 
+               stars = "⭐"; 
             break;
             case 3:
             case 4:
-               stars = ""; 
+               stars = "⭐⭐"; 
             break;
             case 5:
             case 6:
-               stars = ""; 
+               stars = "⭐⭐⭐"; 
             break;
             case 7:
             case 8:
-               stars = ""; 
+               stars = "⭐⭐⭐⭐"; 
             break;
             case 9:
             case 10:
-               stars = ""; 
+               stars = "⭐⭐⭐⭐⭐"; 
             break;
             default:
                 break;
         }
-        return "";
+        return stars;
     }
     public Integer rankRounder(String rank) {
         var intRank = (int) Math.round(Double.parseDouble(rank));
