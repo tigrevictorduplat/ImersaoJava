1eddbadd6fc569dac7506e0f7ebcd2962459ee57
Víctor Duplat Tigre <98099656+tigrevictorduplat@users.noreply.github.com>
2022-08-04 18:55:44 -0300

    Descomentando e Ocultando Chave

--- TierFilms/App.java
+++ TierFilms/App.java
@@ -5,16 +5,15 @@ import java.io.IOException;
 public class App {
     public static void main(String[] args) throws IOException, InterruptedException {
     imdbAPIConnect conexaoAPI = new imdbAPIConnect();
-    /*
+//Listando os Top 250 Melhores Filmes 
     conexaoAPI.listarTopFilmes(
         conexaoAPI.extractAPIBody(
            conexaoAPI.conectAPI(
-               "https://imdb-api.com/en/API/Top250Movies/k_g2rdr7az"+conexaoAPI.getKey())));
-    */
-
+               "https://imdb-api.com/en/API/Top250Movies/"+conexaoAPI.getKey())));
+//Listando os TOP100 Filmes Mais Populares
     conexaoAPI.listarTopFilmes(
         conexaoAPI.extractAPIBody(
             conexaoAPI.conectAPI(
-                "https://imdb-api.com/en/API/MostPopularMovies/k_g2rdr7az"+conexaoAPI.getKey())));
+                "https://imdb-api.com/en/API/MostPopularMovies/"+conexaoAPI.getKey())));
 } 
 }
